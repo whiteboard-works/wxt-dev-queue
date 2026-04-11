a249ffb9529a2eedb3cd2d28634c143a8e2b13ec
Aaron <aaronklinker1@gmail.com>
2026-04-11 15:48:20 -0500

    chore: Fix cors issue due to an async global hook

--- src/plugins/cors-plugin.ts
+++ src/plugins/cors-plugin.ts
@@ -1,7 +1,7 @@
 import { createApp } from "@aklinker1/zeta";
 
 export const corsPlugin = createApp()
-  .onGlobalRequest(async ({ method, set }) => {
+  .onGlobalRequest(({ method, set }) => {
     set.headers["Access-Control-Allow-Origin"] = "*";
     set.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
     set.headers["Access-Control-Allow-Headers"] = "*";
